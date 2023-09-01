64e041b73b6e526951f90443f2e0546041226884
Kuang Yu Heng <yuheng3107@gmail.com>
2023-09-01 17:40:55 +0800

    Fixed overflow and styling issues with JobCard

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,49 +1,74 @@
 import LocationMarker from "./LocationMarker";
 import Button from "./Button";
-export default function JobCard() {
+export default function JobCard({
+  imageURL,
+  organisation,
+  rating,
+  positionTitle,
+  timePosted,
+  location,
+  roleType,
+  skills,
+  salaryRange,
+}: {
+  imageURL: string;
+  organisation: string;
+  rating: number;
+  positionTitle: string;
+  timePosted: string;
+  location: string;
+  roleType: string;
+  skills: string[];
+  salaryRange: string;
+}) {
   return (
-    <div className="w-11/12 bg-white h-44 m-1 rounded-md  shadow-[0_0_2px_rgba(0,0,0,0.25)] flex flex-col p-1">
-      <div className="grid grid-cols-6 h-4/5 mt-2">
-        <div id="logo" className="col-span-1 ">
-          <img src="./GovTech.png" alt="" />
+    <div className="w-[97%] bg-white h-50 m-1 rounded-md  shadow-[0_0_2px_rgba(0,0,0,0.25)] flex flex-col p-1">
+      <div className="grid grid-cols-10 h-4/5 mt-2 py-2 mx-2">
+        <div id="logo" className="col-span-2 ">
+          <img src={imageURL} alt={organisation} />
         </div>
         <div
           id="information"
-          className="col-span-4 flex flex-col justify-center items-start p-1 gap-y-0.5"
+          className="col-span-6 flex flex-col justify-center items-start p-1 gap-y-0.5"
         >
           <div className="flex gap-4 text-sm" id="mid-top">
-            <p>GovTech</p>
-            <p>4 ★ </p>
+            <p>{organisation}</p>
+            <p>{rating} ★ </p>
           </div>
           <div id="mid-mid" className="flex">
-            <p className="text-left text-md font-semibold">
-              Technical Product Manager (Cloud)
-            </p>
+            <p className="text-left text-md font-semibold">{positionTitle}</p>
           </div>
           <div className="mid-second-bottom text-xs flex justify-start items-center gap-2">
-            <p className="text-left text-[#1fc76a] font-bold">
-              less than a minute ago
-            </p>
-            <p className="flex">
+            <p className="text-left text-[#1fc76a] font-bold">{timePosted}</p>
+            <div className="flex">
               <LocationMarker></LocationMarker>
-              <p className="ms-1 text-[#838383]">Singapore</p>
-            </p>
+              <p className="ms-1 text-[#838383]">{location}</p>
+            </div>
           </div>
-          <div
-            id="salary-row"
-            className="flex gap-2 justify-center items-center"
-          >
-            <p id="salary" className="text-sm font-medium">
-              S$12,500 - S$20,250 / mth
-            </p>
-            <Button className="bg-[#F1F1F1] font-normal">EST</Button>
-          </div>
+          {salaryRange && (
+            <div
+              id="salary-row"
+              className="flex gap-2 justify-center items-center"
+            >
+              <p id="salary" className="text-xs font-medium">
+                {salaryRange}
+              </p>
+              <Button className="bg-[#F1F1F1] font-normal">EST</Button>
+            </div>
+          )}
         </div>
-        <div id="role-type" className="">
-          <Button className="text-[#1fc76a] bg-[#DDF7E9]">PM</Button>
+        <div
+          id="role-type"
+          className="col-span-2 flex flex-col flex-grow-0 items-end"
+        >
+          <Button className="text-[#1fc76a] bg-[#DDF7E9]">{roleType}</Button>
         </div>
       </div>
-      <div className="grow-0 border-t-2 mx-4">Hello</div>
+      <div className="border-t-2 mx-4 flex gap-0.5">
+        {skills.map((skill) => (
+          <Button className="bg-[#F1F1F1] text-[#838383] my-1">{skill}</Button>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/JobsPage.tsx
+++ src/components/JobsPage.tsx
@@ -1,14 +1,47 @@
 import JobCard from "./JobCard";
 
+const data = [
+  {
+    imageURL: "./GovTech.png",
+    organisation: "GovTech",
+    rating: 4,
+    positionTitle: "Technical Product Manager (Cloud)",
+    timePosted: "less than a minute ago",
+    location: "Singapore",
+    roleType: "PM",
+    skills: ["play", "IAM"],
+    salaryRange: "",
+  },
+  {
+    imageURL: "./Coupang.png",
+    organisation: "Coupang",
+    rating: 3.9,
+    positionTitle: "Staff, Back-end Engineer (Coupang Pay)",
+    timePosted: "37 minutes ago",
+    location: "Singapore",
+    roleType: "Backend",
+    skills: ["Rest API", "API", "play", "Modular", "Node.js"],
+    salaryRange: "S$12,500 - $20,250 / mth",
+  },
+  {
+    imageURL: "./Embed.png",
+    organisation: "Embed",
+    rating: 3.5,
+    positionTitle: "Senior Software Engineer (.Net)",
+    timePosted: "about 1 hour ago",
+    location: "Singapore",
+    roleType: "Fullstack",
+    skills: ["Rest API", "Docker", "API", "TDD", "JavaScript"],
+    salaryRange: "",
+  },
+];
 export default function JobsPage() {
   return (
     <div className="h-screen flex-col items-center justify-center">
-      <div className="h-1/5"></div>
-      <div className="bg-[#F1F1F1] w-screen h-4/5 flex flex-col items-center pt-4">
-        <JobCard></JobCard>
-        <JobCard></JobCard>
-        <JobCard></JobCard>
-        <JobCard></JobCard>
+      <div className="bg-[#F1F1F1] w-screen h-4/5 flex flex-col items-center pt-4 mx-2">
+        <JobCard {...data[0]}></JobCard>
+        <JobCard {...data[1]}></JobCard>
+        <JobCard {...data[2]}></JobCard>
       </div>
     </div>
   );
